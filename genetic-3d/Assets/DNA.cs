75837370f5e3a6edae9d4128c66b8dc64a47ab3a
Vijayasri Iyer <thisisvij98@gmail.com>
2019-05-29 23:01:10 +0530

    Added second game

---
+++ genetic-3d/Assets/DNA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    List<int> genes = new List<int>;
+    int dnalength = 0;
+    int maxValues = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
