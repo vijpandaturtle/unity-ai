f8f7d0d673cc6c778909651274302ca0e7be0fa2
Vijayasri Iyer <thisisvij98@gmail.com>
2019-05-29 18:54:41 +0530

    Spawned agents
    Genetic algorithm game

---
+++ genetic-algorithm/Assets/PopulationManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject personPrefab;
+    public int populationSize = 10;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    
+    //Initialization
+    void Start()
+    {
+        for (int i=0; i<populationSize; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-7.5f,7.5f), Random.Range(-3.5f,3.5f), 0);
+            GameObject go = Instantiate(personPrefab, pos, Quaternion.identity);
+            Debug.Log(go);
+            Debug.Log("Person Added");
+            go.GetComponent<DNA>().r = Random.Range(0.0f, 1.0f);
+            go.GetComponent<DNA>().g = Random.Range(0.0f, 1.0f);
+            go.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
+            population.Add(go);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
