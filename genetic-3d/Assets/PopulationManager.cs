62fcb5fee076bcb6d2854a84b7231e7a75468899
Vijayasri Iyer <thisisvij98@gmail.com>
2019-05-30 19:43:07 +0530

    Spawned Characters with PopulationManager

---
+++ genetic-3d/Assets/PopulationManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject botPrefab;
+    public int populationSize = 50;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public float trialTime = 5;
+    int generation = 1;
+
+    GUIStyle guiStyle = new GUIStyle();
+    void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 200, 30), "Generation:" + generation, guiStyle);
+        GUI.Label(new Rect(10, 50, 200, 30), string.Format("Time: {0:0.00}" + (int)elapsed), guiStyle);
+        GUI.Label(new Rect(10, 75, 200, 30), "Population:" + population.Count, guiStyle);
+        GUI.EndGroup();
+    }
+
+   /* GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 pos = new Vector3(Random.Range(-7.0f, 7.0f), Random.Range(-3.5f, 3.5f), 0);
+        GameObject offspring = Instantiate(personPrefab, pos, Quaternion.identity);
+        DNA dna1 = parent1.GetComponent<DNA>();
+        DNA dna2 = parent1.GetComponent<DNA>();
+        //50% chance that the offspring get mother of father's data
+        offspring.GetComponent<DNA>().r = Random.Range(0, 10) < 5 ? dna1.r : dna2.r;
+        offspring.GetComponent<DNA>().g = Random.Range(0, 10) < 5 ? dna1.g : dna2.g;
+        offspring.GetComponent<DNA>().b = Random.Range(0, 10) < 5 ? dna1.b : dna2.b;
+        offspring.GetComponent<DNA>().s = Random.Range(0, 10) < 5 ? dna1.b : dna2.b;
+        return offspring;
+    }*/
+
+  /*  void BreedNewPopulation()
+    {
+        List<GameObject> newPopulation = new List<GameObject>();
+        //get rid of the unfit agents 
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<DNA>().timeToDie).ToList();
+        population.Clear();
+
+        //breed upper half of sorted list 
+        for (int i = (int)(sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++)
+        {
+            //to ensure population size stays the same
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+        }
+
+        //destroy all parents and old population 
+        for (int i = 0; i < sortedList.Count; i++)
+        {
+            Destroy(sortedList[i]);
+        }
+        generation++;
+
+    }*/
+
+    //Initialization
+    void Start()
+    {
+        for (int i = 0; i < populationSize; i++)
+        {
+            Vector3 startingPos = new Vector3(this.transform.position.x * Random.Range(-0.5f, 0.5f),
+                                              this.transform.position.y,
+                                              this.transform.position.z * Random.Range(-2, 2));
+
+
+            GameObject b = Instantiate(botPrefab, startingPos, this.transform.rotation);
+            b.GetComponent<Brain>().Init();
+            population.Add(b);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed > trialTime)
+        {
+            //BreedNewPopulation();
+            elapsed = 0;
+        }
+    }
+}
